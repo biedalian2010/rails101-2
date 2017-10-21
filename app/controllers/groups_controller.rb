2b3c76fdc581f190128986eb8cca9bb119d642e5
李阳 <11@11.com>
2017-10-22 00:00:43 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,7 +16,6 @@ class GroupsController < ApplicationController
 
   def show
     @group = Group.find(params[:id])
-
   end
 
   def edit
@@ -26,7 +25,7 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     @group.update(group_params)
-    redirect_to groups_path
+    redirect_to groups_path, notice: "Update Success"
   end
 
   def destroy
